a459dd3b33b89b0c3d7e9feba38a9b6c5c4e9185
千里客 <2812929818@qq.com>
2024-08-05 17:14:30 +0800

    v4.0 更新了电机力矩系数

--- src/livelybot_serial/src/hardware/motor.cc
+++ src/livelybot_serial/src/hardware/motor.cc
@@ -38,16 +38,15 @@ inline int16_t motor::tqe_float2int(float in_data, motor_type motor_type)
         ROS_ERROR("motor type not set,fresh command fault");
         return int16_t();
     case motor_type::_5046:
-        return (int16_t)((in_data + 0.07) / 0.00528);
+        return (int16_t)(in_data * 100); 
     case motor_type::_4538:
         return (int16_t)((in_data + 0.05) / 0.00445);
     case motor_type::_5047_36:
-        // return (int16_t)((in_data + 0.05253) / 0.00462);  // 老款
-        return (int16_t)((in_data + 0.03313) / 0.004938);
+        return (int16_t)(in_data * 100); 
     case motor_type::_5047_9:
         return (int16_t)((in_data + 0.034809) / 0.00533);
     case motor_type::_4438_32:
-        return (int16_t)((in_data + 0.083) / 0.005584f);
+        return (int16_t)(in_data * 100); 
     default:
         ROS_ERROR("motor type setting error");
         return int16_t();
@@ -254,16 +253,15 @@ inline float motor::tqe_int2float(int16_t in_data, motor_type type)
     case (motor_type::null):
         ROS_ERROR("motor type not set,fresh data fault");
     case (motor_type::_5046):
-        return (float)(in_data * 0.005397) - 0.07;
+        return (int16_t)(in_data * 0.01);
     case (motor_type::_4538):
         return (float)(in_data * 0.00445) - 0.05;
     case (motor_type::_5047_36):
-        // return (float)(in_data * 0.00462) - 0.05253;  // 老款
-        return (float)(in_data * 0.004938f) - 0.03313f;
+        return (int16_t)(in_data * 0.01);
     case (motor_type::_5047_9):
         return (float)(in_data * 0.00533) - 0.034809;
     case (motor_type::_4438_32):
-        return (float)(in_data * 0.005584) - 0.083f;
+        return (int16_t)(in_data * 0.01);
     case (motor_type::_4438_8):
         return (float)(in_data * 0.0055); // 未测
     case (motor_type::_7136_7):
